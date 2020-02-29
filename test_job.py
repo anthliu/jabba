ae8a84d36b9089f4f5a4170d31eed74d1159f199
Anthony Liu <anthliu@umich.edu>
2020-02-28 20:15:19 -0500

    add nested referential variablees, add async job runner

--- test_job.py
+++ test_job.py
@@ -6,7 +6,7 @@ import random
 pid = os.getpid()
 args = sys.argv[1:]
 print(f'[{pid}] Running test.py with args {args}.')
-sleeptime = random.randint(1, 15)
+sleeptime = random.randint(1, 5)
 print(f'[{pid}] Sleeping for {sleeptime} seconds.')
 time.sleep(sleeptime)
 print(f'[{pid}] Done.')
