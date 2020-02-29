52bf2c9dbb026ff8d15eb124397fad5476933578
Anthony Liu <anthliu@umich.edu>
2020-02-28 20:33:43 -0500

    hotfix floating point errors in load addition

--- test_job.py
+++ test_job.py
@@ -8,5 +8,7 @@ args = sys.argv[1:]
 print(f'[{pid}] Running test.py with args {args}.')
 sleeptime = random.randint(1, 5)
 print(f'[{pid}] Sleeping for {sleeptime} seconds.')
+if any('fail' in arg for arg in args):
+    raise Exception('[{pid}] Failing on purpose now.')
 time.sleep(sleeptime)
 print(f'[{pid}] Done.')
