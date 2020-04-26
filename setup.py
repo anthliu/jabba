2d666136b9aaf291dbf10bf9ffae8d62592dfb30
Anthony Liu <anthliu@umich.edu>
2020-04-26 16:38:10 -0400

    reorganize, package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='jabbalib',
+    version='0.1.0',
+    author='Anthony Liu',
+    author_email='anthliu@anthliu.edu',
+    py_modules=['jabbalib'],
+    scripts=['bin/jabba','bin/jabbarq'],
+)
